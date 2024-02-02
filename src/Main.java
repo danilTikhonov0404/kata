7fb578151e1b73da8bc5ddccc89dca2195c89cb1
danil <daniltikhonov702@gmail.com>
2024-02-02 17:20:15 +0300

    ваыфа

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner s = new Scanner(System.in);
         System.out.println("Введите выражение: ");
         String vvod = s.nextLine();
@@ -16,45 +17,41 @@ public class Main {
         int resultArab = 0; //Итоговый результат в арабсих числах
         String[] numbersAndOperation; // Массив строк в котром 0-ой элемент это первое число, 1-ый элемент это опрация, 2-й элемент это второе число
         numbersAndOperation = vvod.split(" "); //Разбиваем строку на элементы в массив выше
-        operation =  numbersAndOperation[1].charAt(0); // получаем операцию,берем 1-ый элемент массива, в нем берем 0-ой символ
+        operation = numbersAndOperation[1].charAt(0); // получаем операцию,берем 1-ый элемент массива, в нем берем 0-ой символ
         if (numbersAndOperation.length > 3) { // проверяем на корректность выражения, не ввел ли пользователь лишние буквы, числа, символы
-            System.out.println(" = ? \nЧто-то пошло не так...\nКалькулятор считает только две переменные! Попробуйте снова.");
-            System.exit(0); //если ввел не корректно, то выходим из программы
+            throw new IOException();
         }
-        try{
+        try {
             num1 = romanNumeral(numbersAndOperation[0]);//Если у нас римские цифры,то они будут перведены в орабсие и присвоены соответствующим переменным;
             num2 = romanNumeral(numbersAndOperation[2]);
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println(" = ? \nНет символа для выполнения операции или отсутствует вторая переменная! Попробуйте снова.");
-            System.exit(0); // снова на случай, если пользователь вел не верно , то выходим их программы
+            throw new IOException();
         }
         if (num1 == 0 | num2 == 0) {
             result = 0;
             try {
                 num1 = Integer.parseInt(numbersAndOperation[0]);
                 num2 = Integer.parseInt(numbersAndOperation[2]);
-                if (num1 > 10 | num2 > 10 | num1 < 0 | num2 <0) {
-                    System.out.println(" = ? \nКалькулятор умеет работать только с целыми арабскими или римскими цифрами от 1 до 10 одновременно! Попробуйте снова.");
-                    System.exit(0);
+                if (num1 > 10 | num2 > 10 | num1 < 0 | num2 < 0) {
+                    throw new IOException();
                 }
                 resultArab = calculate(num1, num2, operation);
                 System.out.println(" = " + resultArab);
             } catch (NumberFormatException e) {
-                System.out.println("=? \nКалькулятор умеет работать только с целыми арабскими или римскими цифрами от 1 до 10 одновременно! Попробуйте снова.");
+                throw new IOException();
             } catch (ArithmeticException e) {
-                System.out.println("=? \nНа ноль делить нельзя! Попробуйте снова.");
+                throw new IOException();
             }
         } else {
             try {
                 result = calculate(num1, num2, operation);
                 if (result == 0) {
-                    System.out.println(" = ? \nРезультатом работы калькулятора с римскими числами могут быть только положительные числа! Попробуйте снова.");
-                    System.exit(0);
+                    throw new IOException();
                 }
                 String resultRom = romanSolution(result);
                 System.out.println(" = " + resultRom + " (" + result + ")");
             } catch (ArrayIndexOutOfBoundsException e) {
-                System.out.println(" = ? \nРезультатом работы калькулятора с римскими числами могут быть только положительные числа! Попробуйте снова.");
+                throw new IOException();
             }
         }
     }
